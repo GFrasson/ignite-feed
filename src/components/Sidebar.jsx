6ddf8c8227a278d45f98b4473abfd699ad0b54a7
Gabriel Frasson <bielfc27@gmail.com>
2023-02-02 20:45:26 -0300

    Avatar Component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
     return (
@@ -11,10 +12,7 @@ export function Sidebar() {
             />
         
             <div className={styles.profile}>
-                <img
-                    className={styles.avatar}
-                    src="https://github.com/GFrasson.png"
-                />
+                <Avatar src="https://github.com/GFrasson.png" />
 
                 <strong>Gabriel Frasson</strong>
                 <span>Web Developer</span>
