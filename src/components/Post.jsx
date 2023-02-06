f49af00c1093f7a938a3ce1d108399c838611324
Gabriel Frasson <bielfc27@gmail.com>
2023-02-06 17:13:11 -0300

    Add Comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,8 +5,12 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState(['Post muito bacana!']);
+    const [newCommentText, setNewCommentText] = useState('');
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
     });
@@ -15,6 +19,17 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true
     });
+
+    function handleCreateNewComment(event) {
+        event.preventDefault();
+
+        setComments([...comments, newCommentText]);
+        setNewCommentText('');
+    }
+
+    function handleNewCommentChange(event) {
+        setNewCommentText(event.target.value);
+    }
 
     return (
         <article className={styles.post}>
@@ -47,11 +62,13 @@ export function Post({ author, publishedAt, content }) {
                 }
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
                     placeholder='Escreva um comentário...'
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
                 />
 
                 <footer>
@@ -60,9 +77,14 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {
+                    comments.map(comment => (
+                        <Comment
+                            key={comment}
+                            content={comment}
+                        />
+                    ))
+                }
             </div>
         </article>
     );
